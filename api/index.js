b237910a2093108e222dc8c8c3c6419e608ed3c5
irumshaik19-lgtm <irumshaik19@gmail.com>
2025-12-27 18:42:19 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,25 @@
 
+// api/index.js
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const reelUrl = req.query.url;
+  if (!reelUrl) return res.status(400).json({ error: "No URL provided" });
+
+  try {
+    const response = await axios.get(
+      `https://instagram-reels-downloader-api.p.rapidapi.com/downloadReel?url=${reelUrl}`,
+      {
+        headers: {
+          "X-RapidAPI-Key": "YOUR_KEY_HERE",
+          "X-RapidAPI-Host": "instagram-reels-downloader-api.p.rapidapi.com",
+        },
+      }
+    );
+
+    return res.status(200).json(response.data);
+
+  } catch (error) {
+    return res.status(500).json({ error: "Server Error", details: error.message });
+  }
+}
