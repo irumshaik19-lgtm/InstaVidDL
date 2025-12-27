8e6264378959dcc57dd7ccd7803da845d2f4b336
irumshaik19-lgtm <irumshaik19@gmail.com>
2025-12-27 18:55:47 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,25 +1,31 @@
-
 // api/index.js
 import axios from "axios";
 
 export default async function handler(req, res) {
-  const reelUrl = req.query.url;
-  if (!reelUrl) return res.status(400).json({ error: "No URL provided" });
+  const videoUrl = req.query.url; 
+  if (!videoUrl) {
+    return res.status(400).json({ status: "error", message: "No URL provided" });
+  }
 
   try {
-    const response = await axios.get(
-      `https://instagram-reels-downloader-api.p.rapidapi.com/downloadReel?url=${reelUrl}`,
-      {
-        headers: {
-          "X-RapidAPI-Key": "YOUR_KEY_HERE",
-          "X-RapidAPI-Host": "instagram-reels-downloader-api.p.rapidapi.com",
-        },
+    const options = {
+      method: 'GET',
+      url: 'https://instagram-reels-downloader-api.p.rapidapi.com/download',
+      params: { url: videoUrl },
+      headers: {
+        'x-rapidapi-key': '6b00492dd5msh9acc1e3a9e2b0f2p159605jsncad9b423229c', 
+        'x-rapidapi-host': 'instagram-reels-downloader-api.p.rapidapi.com'
       }
-    );
+    };
 
-    return res.status(200).json(response.data);
+    const response = await axios.request(options);
+    return res.status(200).json({ status: "success", data: response.data });
 
   } catch (error) {
-    return res.status(500).json({ error: "Server Error", details: error.message });
+    return res.status(500).json({
+      status: "error",
+      message: "Server error from API",
+      details: error.message
+    });
   }
 }
